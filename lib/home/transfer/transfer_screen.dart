71f8e59c6b0b3fcaf06013ee6332be101b4beb89
BernardinoGoncalves <bernardo@ucl.br>
2023-05-29 19:21:40 -0300

    grafico da tela de transferencia

--- lib/home/transfer/transfer_screen.dart
+++ lib/home/transfer/transfer_screen.dart
@@ -8,16 +8,68 @@ class TransferScreen extends StatefulWidget {
 }
 
 class _TransferScreenState extends State<TransferScreen> {
+
+TextEditingController email = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Transfer Screen",
-          style: TextStyle(
-            color: Colors.white,
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
           ),
-        ),
+          Image.asset(
+            "images/vtr_logo.png",
+            width: 300,
+          ),  
+          const Center(
+            child: Text("Transferência",
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontWeight: FontWeight.w700)),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextField(
+              controller: email,
+              maxLines: 1, // Permite várias linhas
+              keyboardType: TextInputType.multiline,
+              decoration: const InputDecoration(
+                labelText: 'E-mail',
+                border: OutlineInputBorder(),
+              ),
+            )
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Container(width: MediaQuery.of(context).size.width /2 - 10,
+            height: 54,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.all(Radius.circular(10))),
+            child: InkWell( 
+              onTap: ()  {
+                  
+              },
+              child: const Center(
+                child: Text("Transferir",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w700)),
+              ),
+            )
+          )
+        ]
       ),
     );
   }
