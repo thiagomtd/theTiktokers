b39d75328ed17138da26f442f0c5d3e933258162
Thiago Santos <thiagomtd2@gmail.com>
2023-06-05 16:18:14 -0300

    1. Adicionando notificaçãoes

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vtr_effects/home/about_us/about_us_screen.dart';
@@ -16,7 +17,44 @@ class Home extends StatefulWidget {
 }
 
 class _MainHomeState extends State<Home> {
- @override
+  Future openDialog(String msg) async {
+    print("OK");
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Notificação'),
+        content: Text(msg),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar')),
+        ],
+      ),
+    );
+  }
+
+  void BuscaNofity() async {
+    DocumentSnapshot userDocumentSnapshot = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+    var userDados = (userDocumentSnapshot.data() as Map<String, dynamic>);
+    for (var element in userDados['notify']) {
+      openDialog(element);
+    }
+    userDocumentSnapshot.reference.update({'notify': []});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //openDialog("Teste de notificação");
+    BuscaNofity();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
@@ -30,135 +68,149 @@ class _MainHomeState extends State<Home> {
                 "images/vtr_logo.png",
                 width: 300,
               ),
-              Column(children: [
-                const SizedBox(
-                          height: 50,
-                        ),
-                Row( 
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                  Container(width: MediaQuery.of(context).size.width /2 - 10,
-                            height: 54,
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            child: InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => ProductsScreen(parametro:true)),
-                                );
-                              },
-                              child: const Center(
-                                child: Text("Produtos",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w700)),
-                              ),
-                            )),
-                            Container(width: MediaQuery.of(context).size.width /2 - 10,
-                            height: 54,
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            child: InkWell(
-                              onTap: () {
-                                 Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => HomeScreen()),
-                                );
-                              },
-                              child: const Center(
-                                child: Text("Rede VTR",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w700)),
-                              ),
-                            ))
-                ],),
-                const SizedBox(
-                          height: 15,
-                        ),
-                Row( 
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                  Container(width: MediaQuery.of(context).size.width /2 - 10,
-                            height: 54,
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            child: InkWell(
-                              onTap: () {      
-                                 Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => AboutUsScreen()),
-                                );     
-                              },
-                              child: const Center(
-                                child: Text("Sobre",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w700)),
-                              ),
-                            )),
-                            Container(width: MediaQuery.of(context).size.width /2 - 10,
-                            height: 54,
-                            decoration: const BoxDecoration(
-                                color: Colors.white,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            child: InkWell(
-                              onTap: () {  
-                                 Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => ContactScreen()),
-                                );                              
-                              },
-                              child: const Center(
-                                child: Text("Fale Conosco",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w700)),
-                              ),
-                            ))
-                ],),
-                const SizedBox(
-                          height: 15,
-                        ),
-                Row( 
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width /2 - 10,
-                    height: 54,
-                    decoration: const BoxDecoration(
-                        color: Colors.white,
-                        borderRadius:
-                            BorderRadius.all(Radius.circular(10))),
-                    child: InkWell(
-                      
-                      onTap: () {
-                          FirebaseAuth.instance.signOut();
-                          Get.offAll(LoginScreen);                          
-                      },
-                      child: const Center(
-                        child: Text("Sair",
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w700)),
-                      ),
-                    )
-                  )
-                ],)
-              ],)              
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          width: MediaQuery.of(context).size.width / 2 - 10,
+                          height: 54,
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        ProductsScreen(parametro: true)),
+                              );
+                            },
+                            child: const Center(
+                              child: Text("Produtos",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700)),
+                            ),
+                          )),
+                      Container(
+                          width: MediaQuery.of(context).size.width / 2 - 10,
+                          height: 54,
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomeScreen()),
+                              );
+                            },
+                            child: const Center(
+                              child: Text("Rede VTR",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700)),
+                            ),
+                          ))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                          width: MediaQuery.of(context).size.width / 2 - 10,
+                          height: 54,
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => AboutUsScreen()),
+                              );
+                            },
+                            child: const Center(
+                              child: Text("Sobre",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700)),
+                            ),
+                          )),
+                      Container(
+                          width: MediaQuery.of(context).size.width / 2 - 10,
+                          height: 54,
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ContactScreen()),
+                              );
+                            },
+                            child: const Center(
+                              child: Text("Fale Conosco",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700)),
+                            ),
+                          ))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          width: MediaQuery.of(context).size.width / 2 - 10,
+                          height: 54,
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: InkWell(
+                            onTap: () {
+                              //BuscaNofity();
+                              //openDialog("Teste de notificação");
+                              FirebaseAuth.instance.signOut();
+                              Get.offAll(LoginScreen);
+                            },
+                            child: const Center(
+                              child: Text("Sair",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700)),
+                            ),
+                          ))
+                    ],
+                  ),
+                ],
+              )
             ],
           ),
         ),
--- lib/home/products/products_screen.dart
+++ lib/home/products/products_screen.dart
@@ -5,30 +5,28 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:vtr_effects/home/product/product_screen.dart';
 import 'package:get/get.dart';
 
-
 class ProductsScreen extends StatefulWidget {
   final bool parametro;
   const ProductsScreen({required this.parametro});
-  
 
   @override
-  State<ProductsScreen> createState() => _ProductsScreenState(parametro2:parametro);
+  State<ProductsScreen> createState() =>
+      _ProductsScreenState(parametro2: parametro);
 }
 
 class _ProductsScreenState extends State<ProductsScreen> {
-
   final bool parametro2;
   _ProductsScreenState({required this.parametro2});
 
   TextEditingController productIdController = TextEditingController();
 
-  List<String> itemsUser = [];  
+  List<String> itemsUser = [];
   List<Map<String, dynamic>> itemList = [];
   List<Map<String, dynamic>> itemList2 = [];
 
-  void BuscarItemsDoUser() async{
+  void BuscarItemsDoUser() async {
     itemsUser.clear();
-    if(parametro2){
+    if (parametro2) {
       DocumentSnapshot userDocumentSnapshot = await FirebaseFirestore.instance
           .collection("users")
           .doc(FirebaseAuth.instance.currentUser!.uid)
@@ -45,14 +43,13 @@ class _ProductsScreenState extends State<ProductsScreen> {
 
     BuscarItems();
   }
-  void BuscarItems() async{
 
+  void BuscarItems() async {
     itemList.clear();
 
-    var produtcsDocumentSnapshot = await FirebaseFirestore.instance
-          .collection("produtos").get();
+    var produtcsDocumentSnapshot =
+        await FirebaseFirestore.instance.collection("produtos").get();
     for (var element in produtcsDocumentSnapshot.docs) {
-      
       var item = element.data();
       setState(() {
         itemList.add(item);
@@ -62,169 +59,175 @@ class _ProductsScreenState extends State<ProductsScreen> {
       itemList.sort((a, b) {
         if (itemsUser.contains(a['name']) && !itemsUser.contains(b['name'])) {
           return -1;
-        } else if (!itemsUser.contains(a['name']) && itemsUser.contains(b['name'])) {
+        } else if (!itemsUser.contains(a['name']) &&
+            itemsUser.contains(b['name'])) {
           return 1;
         } else {
           return a['name'].compareTo(b['name']);
         }
       });
     });
-    
   }
-  
-  Future<bool> VerifyProductId (String productId, String prefixId) async {
-    if(!(productId.startsWith(prefixId, 0))) {
+
+  Future<bool> VerifyProductId(String productId, String prefixId) async {
+    if (!(productId.startsWith(prefixId, 0))) {
       return false;
     }
 
     QuerySnapshot querySnapshot =
-      await FirebaseFirestore.instance.collection('users').get();
+        await FirebaseFirestore.instance.collection('users').get();
 
     for (QueryDocumentSnapshot documentSnapshot in querySnapshot.docs) {
-      if (documentSnapshot.exists){
+      if (documentSnapshot.exists) {
         var userDados = (documentSnapshot.data() as Map<String, dynamic>);
         var list = (userDados['produtos'] as List<dynamic>);
-        if(list.any((element) => (element as Map<String, dynamic>)['id'] == productId)){
+        if (list.any((element) =>
+            (element as Map<String, dynamic>)['id'] == productId)) {
           return true;
         }
       }
     }
     return false;
   }
-  
-  void AddNewProduct(String productId, Map<String, dynamic> ProductSelected) async {
-    bool v =  await VerifyProductId(productId, ProductSelected['id']);
-    if(v){      
-      Get.snackbar("Falha", "Produto ja registrado!", duration: const Duration(seconds: 5));
+
+  void AddNewProduct(
+      String productId, Map<String, dynamic> ProductSelected) async {
+    bool v = await VerifyProductId(productId, ProductSelected['id']);
+    if (v) {
+      Get.snackbar("Falha", "Produto ja registrado!",
+          duration: const Duration(seconds: 5));
       return;
     }
     DocumentSnapshot userDocumentSnapshot = await FirebaseFirestore.instance
-          .collection("users")
-          .doc(FirebaseAuth.instance.currentUser!.uid)
-          .get();
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
     var item = <String, dynamic>{};
     item['id'] = productId;
     item['name'] = ProductSelected['name'];
     userDocumentSnapshot.reference.update({
-      'produtos':FieldValue.arrayUnion([item])
+      'produtos': FieldValue.arrayUnion([item])
     });
     BuscarItemsDoUser();
-    Get.snackbar("Parabens", "Novo produto registrado!", duration: const Duration(seconds: 5));
+    Get.snackbar("Parabens", "Novo produto registrado!",
+        duration: const Duration(seconds: 5));
   }
 
-   @override
-    void initState() {
-      super.initState();
-      BuscarItemsDoUser();
-    }
-
-  
-  
   @override
+  void initState() {
+    super.initState();
+    BuscarItemsDoUser();
+  }
 
-   Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Image.asset(
-                "images/vtr_logo.png",
-                width: 300,
-              ),  
-              const Center(
-                child: Text("Produtos",
-                    style: TextStyle(
-                        fontSize: 22,
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontWeight: FontWeight.w700)),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: itemList.length,
-                  itemBuilder: (context, index) {
-                    var item = itemList[index];
-                    return GestureDetector(
-                      onTap: () {
-                        if( itemsUser.contains(item['name'])){
-                          var obj = itemList2.where((element) => element['name'] == item['name']).toList().first;
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => ProductScreen(parametro: item, p2:obj)),
-                          );
-                        }
-                        else{
-                          openDialog(item);
-                        }
-                        
-                      },
-                      onLongPress: () {
-                        launch(item['link'].toString());
-                      },
-                      child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                          child: Container(
-                          color: const Color.fromARGB(255, 19, 24, 0),
-                          padding: const EdgeInsets.all(16.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        backgroundColor: Colors.black,
+        body: Column(children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Image.asset(
+            "images/vtr_logo.png",
+            width: 300,
+          ),
+          const Center(
+            child: Text("Produtos",
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontWeight: FontWeight.w700)),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: itemList.length,
+              itemBuilder: (context, index) {
+                var item = itemList[index];
+                return GestureDetector(
+                  onTap: () {
+                    if (itemsUser.contains(item['name'])) {
+                      var obj = itemList2
+                          .where((element) => element['name'] == item['name'])
+                          .toList()
+                          .first;
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                ProductScreen(parametro: item, p2: obj)),
+                      );
+                    } else {
+                      openDialog(item);
+                    }
+                  },
+                  onLongPress: () {
+                    launch(item['link'].toString());
+                  },
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Container(
+                      color: const Color.fromARGB(255, 19, 24, 0),
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
                             children: [
-                              Row(
-                                children: [
-                                  CircleAvatar(
-                                    radius: 30,
-                                    backgroundImage: NetworkImage(item['img'].toString()),
-                                  ),
-                                  const SizedBox(width: 16.0),
-                                  Text(
-                                    item['name'],
-                                    style: const TextStyle(fontSize: 18.0),
-                                  ),                                  
-                                  const SizedBox(width: 16.0), 
-                                ],
+                              CircleAvatar(
+                                radius: 30,
+                                backgroundImage:
+                                    NetworkImage(item['img'].toString()),
                               ),
-                              if(itemsUser.contains(item['name']))
-                                Image.asset(
-                                  "images/palheta.png",
-                                  width: 25,
-                                ),    
+                              const SizedBox(width: 16.0),
+                              Text(
+                                item['name'],
+                                style: const TextStyle(fontSize: 18.0),
+                              ),
+                              const SizedBox(width: 16.0),
                             ],
                           ),
-                        ),
+                          if (itemsUser.contains(item['name']))
+                            Image.asset(
+                              "images/palheta.png",
+                              width: 25,
+                            ),
+                        ],
                       ),
-                    );
-                  },
-                ),
-              ),
-            ]
-          )      
-    );
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ]));
   }
+
   Future openDialog(Map<String, dynamic> ProductSelected) => showDialog(
-    context: context, 
-    builder: (context) => AlertDialog(
-      title: const Text('Digite o serial do produto'),
-      content: TextField(
-        controller: productIdController,
-        decoration: const InputDecoration(hintText: 'Digite o serial do produto'),
-      ),
-      actions: [
-        TextButton(onPressed: ()
-          {
-            Navigator.of(context).pop(productIdController.text);
-            productIdController.clear();
-          }, 
-          child: const Text('Cancelar')),
-        TextButton(onPressed: (){          
-          AddNewProduct(productIdController.text, ProductSelected);
-          Navigator.of(context).pop(productIdController.text);
-          productIdController.clear();
-        }, child: const Text('Enviar')),
-      ],
-    )
-    );
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Digite o serial do produto'),
+          content: TextField(
+            controller: productIdController,
+            decoration:
+                const InputDecoration(hintText: 'Digite o serial do produto'),
+          ),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(productIdController.text);
+                  productIdController.clear();
+                },
+                child: const Text('Cancelar')),
+            TextButton(
+                onPressed: () {
+                  AddNewProduct(productIdController.text, ProductSelected);
+                  Navigator.of(context).pop(productIdController.text);
+                  productIdController.clear();
+                },
+                child: const Text('Enviar')),
+          ],
+        ),
+      );
 }
